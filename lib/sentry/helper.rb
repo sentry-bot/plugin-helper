e4284cc352635b7b4081f2ca75711cb102caf201
jRiddick <apersson.93@gmail.com>
2015-01-28 10:31:29 +0100

    Added function that turns string into an valid URL

--- lib/sentry/helper.rb
+++ lib/sentry/helper.rb
@@ -1,1 +1,11 @@
 require 'sentry/helper/version'
+
+class String
+  def to_url
+    unless self[/\Ahttp:\/\//] || self[/\Ahttps:\/\//]
+      self.sub! self, "http://#{self}"
+    else
+      self
+    end
+  end
+end
